6adb099a58f6d5ffa9c5cf79ed26ca3b2999fb71
lalywr2000 <12190450@inha.edu>
2023-11-24 00:58:05 +0100

    edit context

--- sample_process.sh
+++ sample_process.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 git clone -b SampleProcess https://github.com/lalywr2000/RTOTA.git
+
 cd RTOTA
 cd SampleProcess
+cd src
+
+for file in ./*.txt; do
+    if [ -f "$file" ]; then
+        sed -i 's/SampleProcess/SampleProcess0/g' "$file"
+    fi
+done
+
+cd ..
 mkdir build
 cd build
 cmake ..
