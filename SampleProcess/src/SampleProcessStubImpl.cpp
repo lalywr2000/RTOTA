4a251f73d548ac57dc1d2852504ee60ac4dd8ad3
lalywr2000 <12190450@inha.edu>
2023-11-24 02:42:35 +0100

    return

--- SampleProcess/src/SampleProcessStubImpl.cpp
+++ SampleProcess/src/SampleProcessStubImpl.cpp
@@ -6,9 +6,16 @@ SampleProcessStubImpl::~SampleProcessStubImpl() { }
 void SampleProcessStubImpl::setInput(const std::shared_ptr<CommonAPI::ClientId> _client, uint8_t _input, setInputReply_t _return)
 {
     sender.OutputPortTargetProxy->setInput(_input * 2, sender.callStatus, sender.returnMessage);
-
-    _return(sender.returnMessage + "\t(via SampleProcess_)");
-
+    
+    if (sender.returnMessage != "")
+    {
+        _return(sender.returnMessage + "\t(via SampleProcess_)");
+    }
+    else
+    {
+        _return("");
+    }
+    
     return;
 }
 
