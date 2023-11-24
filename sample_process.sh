fc3244ee9af956a612bd8087fe5947f95dbb3c54
lalywr2000 <12190450@inha.edu>
2023-11-24 01:34:27 +0100

    script

--- sample_process.sh
+++ sample_process.sh
@@ -6,22 +6,22 @@ cd RTOTA
 cd SampleProcess
 cd src
 
-#for file in *.cpp; do
-#    if [ -f "$file" ]; then
-#        sed -i 's/SampleProcess/SampleProcess0/g' "$file"
-#    fi
-#done
+for file in *.cpp; do
+    if [ -f "$file" ]; then
+        sed -i 's/SampleProcess_/SampleProcess_0/g' "$file"
+    fi
+done
 
-#for file in *.hpp; do
-#    if [ -f "$file" ]; then
-#        sed -i 's/SampleProcess/SampleProcess0/g' "$file"
-#    fi
-#done
+for file in *.hpp; do
+    if [ -f "$file" ]; then
+        sed -i 's/SampleProcess_/SampleProcess_0/g' "$file"
+    fi
+done
 
-#cd ..
-#mkdir build
-#cd build
-#cmake ..
-#make
-#./SampleProcess
+cd ..
+mkdir build
+cd build
+cmake -DPROCESS_NUMBER="0" ..
+make
+./SampleProcess_0
 
